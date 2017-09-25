293b88cdc15206dc42d20c79dbe58d6b7613dc20
hyesun <hyesun.choi@ulikekorea.com>
2017-09-25 16:15:58 +0900

    테이블뷰 수정모드 추

--- TodoList/TodoListViewController.swift
+++ TodoList/TodoListViewController.swift
@@ -13,16 +13,36 @@ var list = [TodoList]()
 class TodoListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var todoListTableView: UITableView!
+    @IBOutlet weak var editButton: UIBarButtonItem!
+    let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(doneButtonTap))
+    
+    // done 버튼 tap시 수정모드 종료
+    @objc
+    func doneButtonTap() {
+        self.navigationItem.leftBarButtonItem = editButton
+        todoListTableView.setEditing(false, animated: true)
+    }
+    
+    @IBAction func editButtonAction(_ sender: Any) {
+        // 리스트 비어있을 때 return
+        guard !list.isEmpty else {
+            return
+        }
+        self.navigationItem.leftBarButtonItem = doneButton
+        todoListTableView.setEditing(true, animated: true)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         todoListTableView.delegate = self
         todoListTableView.dataSource = self
+        
+        doneButton.target = self
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        let userDefaults = UserDefaults.standard
+//        let userDefaults = UserDefaults.standard
 //        userDefaults.set(list, forKey: "todoList")
         todoListTableView.reloadData()
     }
@@ -47,6 +67,12 @@ class TodoListViewController: UIViewController, UITableViewDataSource, UITableVi
         }
         
         return cell
+    }
+    
+    // 수정모드
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        list.remove(at: indexPath.row)
+        todoListTableView.reloadData()
     }
 }
 
